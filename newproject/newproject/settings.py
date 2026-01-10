12ee4336036f8aa63500247c2471aabdc87aef1c
Bhaskar Y Hubale <bhaskaryhubale@gmail.com>
2026-01-10 18:14:48 +0530

    updsate

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -94,9 +94,12 @@ TEMPLATES = [
 # ---------------------
 # DATABASE
 # ---------------------
-if os.environ.get('RENDER'):
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.config(
+        'default': dj_database_url.parse(
+            DATABASE_URL,
             conn_max_age=600,
             ssl_require=True
         )
@@ -108,7 +111,6 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
 # ---------------------
 # PASSWORD VALIDATION
 # ---------------------
