0b95b0e8d5d701ba143061f288d31dba46e8b986
Bhaskar Y Hubale <bhaskaryhubale@gmail.com>
2026-02-21 15:06:20 +0530

    ADD

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -171,9 +171,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'bhaskaryhubale.899@gmail.com'
-EMAIL_HOST_PASSWORD = 'liox giwz dlvz mpov'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 # ---------------------
 # DEFAULT FIELD TYPE
 # ---------------------
