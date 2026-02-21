0d6dce3b7e5b6f94a926bbecee56f0ba90e836a4
Bhaskar Y Hubale <bhaskaryhubale@gmail.com>
2026-02-21 15:28:58 +0530

    ADD9

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -135,7 +135,6 @@ USE_TZ = True
 # STATIC & MEDIA FILES
 # ---------------------
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
