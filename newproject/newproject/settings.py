2929dddc36fcd137aa9e86db93559a3f5ae6b08c
Bhaskar Y Hubale <bhaskaryhubale@gmail.com>
2026-02-22 12:52:13 +0530

    fix: remove exposed secrets from settings

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -75,11 +75,7 @@ TEMPLATES = [
     },
 ]
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
-}
+
 # ---------------------
 # DATABASE
 # ---------------------
@@ -151,14 +147,13 @@ RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRET', 'QD36AGTlyzRhuwdLVzi
 # EMAIL CONFIGURATION  ✅ FIXED
 # ---------------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'bhaskaryhubale.899@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'liox giwz dlvz mpov')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'bhaskaryhubale.899@gmail.com')
 
 # ---------------------
 # DEFAULT FIELD TYPE
