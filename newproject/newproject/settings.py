6f737a2798f4f78fabda77b4c036bc1890aceaae
Bhaskar Y Hubale <bhaskaryhubale@gmail.com>
2026-03-12 22:11:12 +0530

    ADD

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -163,9 +163,9 @@ RAZORPAY_KEY_SECRET = "QD36AGTlyzRhuwdLVziqWNUj"
 # settings.py mein ye lines replace karein
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True           # TLS ki jagah SSL use karein
+EMAIL_USE_TLS = False
 # In variables ke naam wahi rakhein jo Render environment mein hain
 EMAIL_HOST_USER = os.environ.get('BREVO_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('BREVO_PASSWORD')
